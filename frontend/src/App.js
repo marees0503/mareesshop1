4d994570d5a3ffdbc9eefb19faa5d8ec8ce6e507
marees0503 <mareeswaranbemech@gmail.com>
2022-11-18 16:36:41 +0530

    list produts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -6,8 +7,27 @@ function App() {
       <header >
         <a href="/">Mareesshop</a>
       </header>
+      <main>list products</main>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
